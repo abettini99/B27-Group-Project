e3ca302b38c6d2d7a6e432c46d95885876d11a4a
jeroenriess <61149358+jeroenriess@users.noreply.github.com>
2020-03-09 15:48:49 +0100

    yo

--- CGcalcs.py
+++ CGcalcs.py
@@ -6,9 +6,6 @@ FFr = data['rh_engine_FMF']
 FFl = data['lh_engine_FMF']
 
 
-
-
-
 def CGshift(Mi, CGi, Mf, moment_change):
     CG = (Mi*CGi + moment_change)/Mf
     
@@ -18,19 +15,17 @@ def CG_MAC(CG_datum):
     CG = (CG_datum - LEMAC)/MAC *100
     return CG
 
-
+#-------------------------------
 MAC = 2.0569 #[m]
 LEMAC = 6.64083 #[m]
 
-# calculate BEM
-#--taken from weight measurements
+
+#--Basic empty mass (BEM) taken from weight measurements
 BEM = 9165 #[lbs]
 CG_BEM = 291.647954 #inches
 
 
-
-#Calculate Payload weight:
-#--using passenger/payload data
+#--Payload weight using passenger/payload data
 x_seats = [131,131,214,214,251,251,288,288,170]
 w_seats = [180,180,170,187,190,180,178,180,190]
 
@@ -56,11 +51,33 @@ RW = OEW + Initial_fuel
 
 CG_RW = CGshift(OEW, CG_OEW, RW, mom_fuel)
 
-
 #Calculate CG at specific time:
 #--Parse fuel-time data
 #--calcukate fuel moment using interpolated fuel-moment data
 #--Calc new CG
+
+def CG_time(t):
+    fuel_weight = Initial_fuel - totalfuelused(t)
+    fuel_mom = interpolatefuel(fuel_weight)
+    tot_weight = OEW+ fuel_weight
+    CG = CGshift(OEW, CG_OEW, tot_weight, fuel_mom)
+    
+    return CG, tot_weight
+
+
+cgg = []
+w = []
+for t in time[0:1000]:
+    cg, weight = CG_time(t)
+    cgg.append(cg)
+    w.append(weight)
+plt.plot(time,cg)
+plt.plot(time, w)
+plt.show()
+
+
+
+
 
 
 
