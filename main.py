1b9c649e887d321400a480663578c66fa7c94c21
Changkyu Park <parkchangkyu.96@gmail.com>
2020-03-23 23:26:02 +0100

    change in main for formula of eigenvalue

--- main.py
+++ main.py
@@ -529,14 +529,14 @@ for motion in ['phugoid', 'shortperiod', 'aperiodicroll', 'dutchroll', 'dutchrol
         # ==============================================================================================
         # Calculate analytical eigenvalues
         # ==============================================================================================
-        Atilde = -1 * (2 * muc) * (CZadot - 2 * muc) * (2 * muc * KY2**2)
+        Atilde = -1 * (2 * muc) * (CZadot - 2 * muc) * (2 * muc * KY2)
 
-        Btilde = ( CXu * (CZadot - 2 * muc) * 2 * muc * KY2**2 - 2 * muc * CZa * 2 * muc * KY2 \
+        Btilde = ( CXu * (CZadot - 2 * muc) * 2 * muc * KY2 - 2 * muc * CZa * 2 * muc * KY2 \
             + 2 * muc * (CZadot - 2 * muc) * Cmq - (CZq + 2 * muc) * Cmadot * 2 * muc )
 
         Ctilde = ( CXu * CZa * (2 * muc * KY2) - CXu * (CZadot - 2 * muc) * Cmq + 2 * muc * CZa * Cmq \
             - CZu * Cmadot * CXq + CXq * (CZadot - 2 * muc) * Cmu - (CZq + 2 * muc) * Cma * 2 * muc \
-            + (CZq + 2 * muc) * Cmadot * CXu - 2 * muc * KY2**2 * CXa * CZu+ CX0 * Cmadot * 2 * muc)
+            + (CZq + 2 * muc) * Cmadot * CXu - 2 * muc * KY2 * CXa * CZu+ CX0 * Cmadot * 2 * muc)
 
         Dtilde = ( -1 * CXu * CZa * Cmq - CZu * Cma * CXq - Cmu * CXa * (CZq + 2 * muc) + CXq * CZa * Cmu \
             + (CZq + 2 * muc) * Cma * CXu + Cmq * CXa * CZu - CZu * Cmadot * CZ0 + CZ0 * (CZadot - 2 * muc) * Cmu \
@@ -676,9 +676,9 @@ for motion in ['phugoid', 'shortperiod', 'aperiodicroll', 'dutchroll', 'dutchrol
             # ==============================================================================================
             # Compute analytical eigenvalues for short period eigenmotion
             # ==============================================================================================
-            Atilde = -1 * (CZadot - 2 * muc) * 2 * muc * KY2**2
+            Atilde = -1 * (CZadot - 2 * muc) * 2 * muc * KY2
 
-            Btilde = ( -1 * CZa * 2 * muc * KY2**2 + (CZadot - 2 * muc) * Cmq + (CZq + 2 * muc) * Cmadot )
+            Btilde = ( -1 * CZa * 2 * muc * KY2 + (CZadot - 2 * muc) * Cmq + (CZq + 2 * muc) * Cmadot )
 
             Ctilde = ( -1 * CZa * Cmq - CX0 * Cmadot+ (CZq + 2 * muc) * Cma )
 
@@ -697,22 +697,22 @@ for motion in ['phugoid', 'shortperiod', 'aperiodicroll', 'dutchroll', 'dutchrol
         # ==============================================================================================
         # Calculate analytical eigenvalues for each eigenmotion
         # ==============================================================================================
-        Atilde = 0.5 * ( -1 * (CYbdot - 2 * mub) * 4 * mub * KX2**2 * 4 * mub * KZ2**2 \
+        Atilde = 0.5 * ( -1 * (CYbdot - 2 * mub) * 4 * mub * KX2 * 4 * mub * KZ2 \
             + (4 * mub * KXZ)**2 * (CYbdot - 2 * mub) )
 
-        Btilde = 0.5 * ( -1 * CYb * 4 * mub * KX2**2 * 4 * mub * KZ2**2 + (CYbdot - 2 * mub) * Clp * 4 * mub  * KZ2**2 \
-               + (CYbdot - 2 * mub) * 4 * mub * KX2 **2 * Cnr - Cnbdot * CYp * 4 * mub * KXZ \
-               - (CYr - 4 * mub) * 4 * mub * KX2**2 * Cnbdot + Clr * 4 * mub * KXZ * (CYbdot - 2 * mub)
+        Btilde = 0.5 * ( -1 * CYb * 4 * mub * KX2 * 4 * mub * KZ2 + (CYbdot - 2 * mub) * Clp * 4 * mub  * KZ2 \
+               + (CYbdot - 2 * mub) * 4 * mub * KX2 * Cnr - Cnbdot * CYp * 4 * mub * KXZ \
+               - (CYr - 4 * mub) * 4 * mub * KX2 * Cnbdot + Clr * 4 * mub * KXZ * (CYbdot - 2 * mub)
                + 4 * mub * KXZ * Cnp * (CYbdot - 2 * mub) + (4 * mub * KXZ)**2 * CYb )
 
-        Ctilde = ( 0.5 * ( CYb * Clp * 4 * mub * KZ2 ** 2 + CYb * 4 * mub * KX2**2 * Cnr - (CYbdot - 2 * mub) * Clp * Cnr \
+        Ctilde = ( 0.5 * ( CYb * Clp * 4 * mub * KZ2 + CYb * 4 * mub * KX2 * Cnr - (CYbdot - 2 * mub) * Clp * Cnr \
                 - Clb * 4 * mub * KXZ * (CYr - 4 * mub) - Cnb * CYp * 4 * mub * KXZ - Cnbdot * CYp * Clr \
-                + (CYr - 4 * mub) * Clp * Cnbdot - (CYr - 4 * mub) * 4 * mub * KX2**2 * Cnb + Clr * Cnp * (CYbdot - 2 * mub) \
-                + Clr * 4 * mub * KXZ * CYb + 4 * mub * KXZ * Cnp * CYb - 4 * mub * KZ2**2 * CYp * Clb ) \
+                + (CYr - 4 * mub) * Clp * Cnbdot - (CYr - 4 * mub) * 4 * mub * KX2 * Cnb + Clr * Cnp * (CYbdot - 2 * mub) \
+                + Clr * 4 * mub * KXZ * CYb + 4 * mub * KXZ * Cnp * CYb - 4 * mub * KZ2 * CYp * Clb ) \
                 - Cnbdot * CL * 4 * mub * KXZ )
 
         Dtilde = ( -0.5 * ( -1 * CYb * Clp * Cnr - Clb * Cnp * (CYr - 4 * mub) - Cnb * CYp * Clr + (CYr - 4 * mub) * Clp * Cnb \
-                + Clr * Cnp * CYb + Cnr * CYp * Clb) - ( Cnb * CL * 4 * mub * KXZ + Cnbdot * CL * Clr + 4 * mub * KZ2**2 * CL * Clb ) )
+                + Clr * Cnp * CYb + Cnr * CYp * Clb) - ( Cnb * CL * 4 * mub * KXZ + Cnbdot * CL * Clr + 4 * mub * KZ2 * CL * Clb ) )
 
         Etilde = (-1 * Cnb * CL * Clr + Cnr * CL * Clb)
 
