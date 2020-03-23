24210fda0db0041e8d052a943a282825aa78e5eb
Changkyu Park <parkchangkyu.96@gmail.com>
2020-03-23 15:00:42 +0100

    minute-ly commit

--- eigenvalue_analytic.py
+++ eigenvalue_analytic.py
@@ -5,7 +5,13 @@ Created on Mon Mar 23 14:29:19 2020
 @author: chang
 """
 
-from main import A, B, C, D
+from main import *
 
 
-print(A,B,C,D)
+A = -1 * (2 * muc) * (CZa - 2 * muc) * (2 * muc * KY2**2)
+
+B = CXu * (CZa - 2 * muc) * 2 * muc * KY2**2 - 2 * muc * CZa * 2 * muc * KY2 + 2 * mu * (CZa - 2 * muc) * Cmq - (CZq + 2 * muc) * Cma * 2 * muc
+
+C = CXu * CZa * (2 * muc * KY2) - CXu * (CZa - 2 * muc) * Cmq + 2 * muc * CZa * Cmq - CZu * Cma * CXq 
+    + CXq * (CZa - 2 * muc) * Cmu - (CZq + 2 * muc) * Cma * 2 * muc + (CZq + 2 * muc) * Cma * CXu - 2 * muc * KY2**2 * CXa * CZu
+    + CX0 * Cma
